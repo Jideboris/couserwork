7205a67c434c8a1e0e9cce5eb4e350cd73477f86
Borisade <olajide_anthony.borisade@bp.com>
2018-02-01 11:28:35 +0000

    fixes

--- Trent/routes/processor.js
+++ Trent/routes/processor.js
@@ -10,20 +10,26 @@ const filePathCert = path.resolve('./public/keys', 'server' + '.key')
 const algoritmn = 'AES-256-CBC';
 const hmacalgo = 'SHA256';
 
-function encryptwithsymmetrickey(data, symmetric_key) {
+function encryptwithsymmetrickey(data, symmetric_key, hmackey) {
 
-    var IV = new Buffer(crypto.randomBytes(16));
+    var IV = new Buffer(crypto.randomBytes(16))
     var cipher_text;
+    var hmac;
     var encryptor;
-
+    console.log('here1')
     encryptor = crypto.createCipheriv(algoritmn, symmetric_key, IV);
-    encryptor.setEncoding('hex')
-    encryptor.write(data)
-    encryptor.end()
+    console.log('here2')
+    encryptor.setEncoding('hex');
+    encryptor.write(data);
+    encryptor.end();
+    console.log('here')
+    cipher_text = encryptor.read();
 
-    cipher_text = encryptor.read()
+    hmac = crypto.createHmac(hmacalgo, hmackey);
+    hmac.update(cipher_text);
+    hmac.update(IV.toString('hex'));
 
-    return cipher_text + "|" + IV.toString('hex')
+    return cipher_text + "$" + IV.toString('hex') + "$" + hmac.digest('hex')
 };
 
 function decryptwithsymmetrickey(ecrypteddata, symmetric_key) {
@@ -47,11 +53,12 @@ function decryptwithsymmetrickey(ecrypteddata, symmetric_key) {
 
 
 };
-function storepublickeys(req, res){
+
+function storepublickeys(req, res) {
     try {
         let key = req.body.key
-        let from = req.body.from     
-        const publickeypath = path.resolve('./public/keys', from + '.cert')      
+        let from = req.body.from
+        const publickeypath = path.resolve('./public/keys', from + '.cert')
         fs.writeFileSync(publickeypath, key)
 
         res.status(200).send()
@@ -67,18 +74,29 @@ function preparestorepublickeys(req, res) {
         let key = req.body.key
         let from = req.body.from
         let to = req.body.to
-
+        console.log(to)
         const publickeypath = path.resolve('./public/keys', from + '.cert')
         const sessionkeypath = path.resolve('./public/keys', from + '.key')
+        const targetpublickey = path.resolve('./public/keys', to + '.cert')
+
         fs.writeFileSync(publickeypath, key)
 
         let sessionkey = generateSymmetricKey()
         fs.writeFileSync(sessionkeypath, sessionkey)
+        let frompublickey = fs.readFileSync(publickeypath)
+        //encrypt bob's public key and bob identity
+        let topublickey = fs.readFileSync(targetpublickey)
+        let publickeyandbobidentity = topublickey + '|' + to
+        let testing = Buffer.from('publickeyandbobidentity', 'utf8');
+        console.log(testing)
+        let tobeforwardedtobob = encryptwithsymmetrickey(testing, sessionkey, sessionkey)
+        //let foralicetodescryptandforward = tobeforwardedtobob + '|' + sessionkey
+        // let encryptforalicetodecryt = encryptwithpublickey(key, foralicetodescryptandforward)
+        let todecrypted = decryptwithsymmetrickey(publickeyandbobidentity, sessionkey)
+        console.log(tobeforwardedtobob)
+        console.log(todecrypted)
 
-        //
-
-        console.log(sessionkey)
-        res.status(200).send(sessionkey)
+        res.status(200).send(tobeforwardedtobob)
 
     } catch (err) {
         console.log('error----->' + err)
@@ -92,7 +110,7 @@ function generateresponse(encryptedtopublickeyandtoidentity, frompublickey) {
 }
 
 function generateSymmetricKey() {
-    var generatedKey = crypto.randomBytes(32).toString('base64');
+    var generatedKey = crypto.randomBytes(32); //.toString('base64');
     return generatedKey;
 
 }
@@ -117,6 +135,6 @@ function encryptwithpublickey(publicKey, data) {
 
 module.exports = {
     preparestorepublickeys: preparestorepublickeys,
-    storepublickeys:storepublickeys
+    storepublickeys: storepublickeys
 
 }
